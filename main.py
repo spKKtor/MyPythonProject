68c2356c5601d436a894b665d612e679cd719a55
spKKtor <stasiik0710@gmail.com>
2024-10-12 12:00:15 +0300

    sho

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#zapusk
+if __name__ == '__main__':
+    app.run(debug=True)
